40729c278d934073466270b92ab853d7ac0e81b9
Smokeyk00 <30637205+Smokeyk00@users.noreply.github.com>
2025-05-05 12:52:56 +0200

    Update install-creality.sh

--- install/install-creality.sh
+++ install/install-creality.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 tz="Etc/UTC"  # FIX: Sätt tz innan install.func använder den
-
-# Copyright...
-# ...
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/install.func)
 CACHER="${CACHER:-false}"  # Sätter standardvärde på CACHER om den inte är definierad
-
+RANDOM_UUID=$(uuidgen)
+# Aktivera verbose-läge och stoppa vid fel
+set -x
+set -e
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: Smokeyk00
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
